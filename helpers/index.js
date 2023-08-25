d4fe8e8ba1c5927c3620075bcdebd427a5196e8f
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-08-26 00:08:56 +0200

    add handleSaveError and handleUpdateValidator

--- helpers/index.js
+++ helpers/index.js
@@ -2,3 +2,4 @@ export { HttpError } from "./HttpError.js";
 export { ctrlWrapper } from "./ctrlWrapper.js";
 export { handleMongooseError } from "./handleMongooseError.js";
 export { sendEmail } from "./sendEmail.js";
+export { handleSaveError, handleUpdateValidator } from "./hooks.js";
