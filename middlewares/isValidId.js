6b811e50e0b7f89d8fdfadd4addf84cb2759391e
Inna Nomerovkina <inkosof@gmail.com>
2023-08-25 15:25:27 +0300

    update middlewares/isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -2,10 +2,10 @@ import { isValidObjectId } from "mongoose";
 import { HttpError } from "../helpers/index.js";
 
 const isValidId = (req, res, next) => {
-	const { contactId } = req.params;
+	const { id } = req.params;
 
-	if (!isValidObjectId(contactId)) {
-		next(HttpError(400, `${contactId} is not valid Id`));
+	if (!isValidObjectId(id)) {
+		next(HttpError(400, `${id} is not valid Id`));
 	}
 	next();
 };
