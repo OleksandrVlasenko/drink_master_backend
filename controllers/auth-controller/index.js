2c2351a4bc4818a13d98bc04c8278d5fa568d383
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-08-25 23:11:30 +0200

    add sendEmail func for verifycation email

--- controllers/auth-controller/index.js
+++ controllers/auth-controller/index.js
@@ -2,16 +2,18 @@ import { getCurrentUser } from "./getCurrentUser.js";
 import { signIn } from "./signIn.js";
 import { logout } from "./logout.js";
 import { signUp } from "./signUp.js";
+import { verify } from "./verify.js";
 import { updateUser } from "./updateUser.js";
 
 import { ctrlWrapper } from "../../helpers/index.js";
 
 const authController = {
-  getCurrentUser: ctrlWrapper(getCurrentUser),
-  signIn: ctrlWrapper(signIn),
-  logout: ctrlWrapper(logout),
-  signUp: ctrlWrapper(signUp),
-  updateUser: ctrlWrapper(updateUser),
+	getCurrentUser: ctrlWrapper(getCurrentUser),
+	signIn: ctrlWrapper(signIn),
+	logout: ctrlWrapper(logout),
+	signUp: ctrlWrapper(signUp),
+	verify: ctrlWrapper(verify),
+	updateUser: ctrlWrapper(updateUser),
 };
 
 export { authController };
