08d526eda2d4abd5a28b34e3998526e1b4bc4c92
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-08-25 19:43:55 +0300

    Joi + Mongoose , 2 hooks ,  getAll - controller

---
+++ controllers/ingredientsList-controller/getAll.js
@@ -0,0 +1,8 @@
+import Ingredient from "../../models/ingredients.js";
+
+const getAll = async (req, res) => {
+  const data = "some Ingredient data";
+  res.json(data);
+};
+
+export { getAll };
--- controllers/ingredientsList-controller/index.js
+++ controllers/ingredientsList-controller/index.js
@@ -1,9 +1,12 @@
 import { getIngredientsList } from "./getIngredientsList.js";
+import { getAll } from "./getAll";
 
 import { ctrlWrapper } from "../../helpers/index.js";
 
 const ingredientsListController = {
 	getIngredientsList: ctrlWrapper(getIngredientsList),
+	getAll: ctrlWrapper(getAll),
+	
 };
 
 export { ingredientsListController };
