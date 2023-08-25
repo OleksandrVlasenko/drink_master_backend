459ce2e3874e3a44d38eb9f2b48d27c1713b0db0
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-08-25 21:25:59 +0200

    add ".js" for "hooks" and "getAll"

--- controllers/ingredientsList-controller/index.js
+++ controllers/ingredientsList-controller/index.js
@@ -1,12 +1,11 @@
 import { getIngredientsList } from "./getIngredientsList.js";
-import { getAll } from "./getAll";
+import { getAll } from "./getAll.js";
 
 import { ctrlWrapper } from "../../helpers/index.js";
 
 const ingredientsListController = {
-	getIngredientsList: ctrlWrapper(getIngredientsList),
-	getAll: ctrlWrapper(getAll),
-	
+  getIngredientsList: ctrlWrapper(getIngredientsList),
+  getAll: ctrlWrapper(getAll),
 };
 
 export { ingredientsListController };
--- models/ingredients.js
+++ models/ingredients.js
@@ -1,11 +1,11 @@
 import { Schema, model } from "mongoose";
 import Joi from "joi";
 
-import { handleSaveError, handleUpdateValidate } from "./hooks"
+import { handleSaveError, handleUpdateValidate } from "./hooks.js";
 
 const ingredientSchema = new Schema(
-    {},
-    { versionKey: false, timestamps: true }
+  {},
+  { versionKey: false, timestamps: true }
 );
 
 ingredientSchema.pre("findByIdAndUpdate", handleUpdateValidate);
@@ -15,14 +15,14 @@ ingredientSchema.post("save", handleSaveError);
 ingredientSchema.post("findByIdAndUpdate", handleSaveError);
 
 const emptySchema = Joi.object()
-    .min(1)
-    .messages({ "object.min": "Missing fields" });
+  .min(1)
+  .messages({ "object.min": "Missing fields" });
 
 const isValidId = Joi.object({
-    favorite: Joi.boolean().required().messages({
-        "any.required": "missing field favorite",
-        "boolean.base": "Must be boolean type",
-    }),
+  favorite: Joi.boolean().required().messages({
+    "any.required": "missing field favorite",
+    "boolean.base": "Must be boolean type",
+  }),
 });
 
 const ingredientSchemaJoi = Joi.object({});
@@ -30,8 +30,8 @@ const ingredientSchemaJoi = Joi.object({});
 const Ingredient = model("ingredient", ingredientSchema);
 
 export default {
-    Ingredient,
-    emptySchema,
-    ingredientSchemaJoi,
-    isValidId,
+  Ingredient,
+  emptySchema,
+  ingredientSchemaJoi,
+  isValidId,
 };
