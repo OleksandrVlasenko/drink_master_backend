064a008c4343edc7972c72debb823d1f12774835
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-08-26 17:22:11 +0200

    remove clg

--- controllers/auth-controller/signIn.js
+++ controllers/auth-controller/signIn.js
@@ -5,17 +5,14 @@ import "dotenv/config";
 import jwt from "jsonwebtoken";
 
 const { SECRET_KEY } = process.env;
-console.log(SECRET_KEY);
 
 export const signIn = async (req, res) => {
 	const { email, password } = req.body;
 	const user = await User.findOne({ email });
-	console.log(user);
 
 	if (!user) throw HttpError(401, "Email or password invalid");
 
 	const passwordCompare = await bcrypt.compare(password, user.password);
-	console.log(passwordCompare);
 	if (!passwordCompare) throw HttpError(401, "Email or password invalid");
 	// if (!user.verify) throw HttpError(404, "User not found");
 
@@ -24,7 +21,6 @@ export const signIn = async (req, res) => {
 	};
 
 	const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-	console.log(token);
 	await User.findByIdAndUpdate(user._id, { token });
 	res.json({
 		token,
