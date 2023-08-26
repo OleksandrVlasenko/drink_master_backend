b77d0e66e454e4ad11a119d917809ba835e0cb71
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-08-26 14:32:20 +0300

    added swagger and part of coctail schema

--- controllers/coctail-controller/getAll.js
+++ controllers/coctail-controller/getAll.js
@@ -1,7 +1,15 @@
 import { Coctail } from "../../models/coctail.js";
 
 async function getAll(req, res) {
-	const result = await Coctail.find();
+	const { page = 1, limit = 10 } = req.query;
+	console.log("getAll  limit:", limit);
+	console.log("getAll  page:", page);
+	const skip = (page - 1) * limit;
+	const result = await Coctail.find({}, "", {
+		skip,
+		limit,
+	}).populate("drink category");
+	console.log("getAll  result:", result);
 	res.json(result);
 }
 
