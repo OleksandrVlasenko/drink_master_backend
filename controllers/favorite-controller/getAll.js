b17ebc6a5d990a839c162e90f6845692430d61c9
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-08-28 00:36:45 +0300

    added pagination to favorite

--- controllers/favorite-controller/getAll.js
+++ controllers/favorite-controller/getAll.js
@@ -3,8 +3,13 @@ import { responseItems } from "../../constants/controllers-constants.js";
 
 async function getAll(req, res) {
 	const { _id: userId } = req.user;
+	const { page = 1, limit = 10 } = req.query;
+	const skip = (page - 1) * limit;
 
-	const result = await Coctail.find({ users: userId }, responseItems);
+	const result = await Coctail.find({ users: userId }, responseItems, {
+		skip,
+		limit,
+	});
 
 	res.json(result);
 }
