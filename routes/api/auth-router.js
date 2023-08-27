98fd804cb2817be68dcf70a03ebba28484ef61d5
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-08-28 01:31:16 +0200

    add router "avatars"

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authController } from "../../controllers/auth-controller/index.js";
-import { validateBody, authenticate } from "../../middlewares/index.js";
+import { validateBody, authenticate, upload } from "../../middlewares/index.js";
 import { userSingUpSchema, userSingInSchema } from "../../models/user.js";
 
 const router = express.Router();
@@ -13,6 +13,6 @@ router.get("/current", authenticate, authController.getCurrentUser);
 
 router.post("/signout", authenticate, authController.signOut);
 
-// router.patch("/", authController.updateUser);
+router.patch("/avatars", authenticate, upload.single("avatar"));
 
 export { router };
