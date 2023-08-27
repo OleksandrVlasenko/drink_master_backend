79aaff2648a6c1ab01f1b70c6caa942d9ad6f28f
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-08-27 18:46:09 +0300

    change-some-routes-to-private

--- routes/api/coctail-router.js
+++ routes/api/coctail-router.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { isValidId } from "../../middlewares/index.js";
 import { coctailController } from "../../controllers/coctail-controller/index.js";
+import { authenticate } from "../../middlewares/index.js";
 
 const router = express.Router();
 
-router.get("/", coctailController.getAll);
+router.get("/", authenticate, coctailController.getAll);
 
-router.get("/:id", isValidId, coctailController.getById);
+router.get("/:id", authenticate, isValidId, coctailController.getById);
 
 export { router };
