0034a4bdd14b966c6bd202b8a1d258997db81009
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-08-28 15:34:18 +0200

    add doc in swager for router "avatars"

--- controllers/auth-controller/updateUserAvatar.js
+++ controllers/auth-controller/updateUserAvatar.js
@@ -1,8 +1,9 @@
-import { cloudinary } from "../../helpers/index.js";
+import { HttpError, cloudinary } from "../../helpers/index.js";
 import { User } from "../../models/user.js";
 import fs from "fs/promises";
 
 const updateUserAvatar = async (req, res) => {
+	if (!req.file) throw HttpError(400, "Please, add your avatar file");
 	const { _id } = req.user;
 	const { path: pathToAvatar } = req.file;
 	const { url: updateAvatarURL } = await cloudinary.uploader.upload(
