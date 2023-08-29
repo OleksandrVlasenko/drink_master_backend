80c403dd580bd91330b79d86cb706f82780c5f71
ArtemBielyi <kolhozreshaet@gmail.com>
2023-08-29 16:27:34 +0300

    add paginations,add  "unique: true" in model/coctail

--- controllers/myRecipes-controller/getAll.js
+++ controllers/myRecipes-controller/getAll.js
@@ -1,3 +1,13 @@
-async function getAll(req, res) {}
+import { Coctail } from "../../models/coctail.js";
+
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const results = await Coctail.find({ owner }).skip(skip).limit(limit);
+  res.json(results);
+};
 
 export { getAll };
