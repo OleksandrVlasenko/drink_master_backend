0ad7b1bf1cde7d62394849635052d5ac20c11b56
ArtemBielyi <kolhozreshaet@gmail.com>
2023-08-29 15:11:59 +0300

    my recipe : browse, add, remove

--- routes/api/myRecipes-router.js
+++ routes/api/myRecipes-router.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { myRecipesController } from "../../controllers/myRecipes-controller/index.js";
+import { authenticate } from "../../middlewares/authenticate.js";
 
 const router = express.Router();
 
-router.post("/", myRecipesController.add);
+router.post("/", authenticate, myRecipesController.add);
 
-router.post("/:id", myRecipesController.removeById);
+router.post("/:id", authenticate, myRecipesController.removeById);
 
-router.get("/", myRecipesController.getAll);
-
+router.get("/", authenticate, myRecipesController.getAll);
 
 export { router };
