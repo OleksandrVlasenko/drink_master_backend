2363b9b0c03da947e800e1d241e6d64aa7de4048
ArtemBielyi <kolhozreshaet@gmail.com>
2023-08-30 17:48:32 +0300

    add doc swagger,fix remove,add

--- controllers/myRecipes-controller/add.js
+++ controllers/myRecipes-controller/add.js
@@ -2,17 +2,17 @@ import { Coctail } from "../../models/coctail.js";
 import { HttpError } from "../../helpers/HttpError.js";
 
 const add = async (req, res) => {
-  const { _id: owner } = req.user;
+	const { _id: owner } = req.user;
 
-  const { drink } = req.body;
-  const existingRecipe = await Coctail.findOne({ drink });
+	const { drink } = req.body;
+	const existingRecipe = await Coctail.findOne({ drink });
 
-  if (existingRecipe) {
-    throw HttpError(409, `${drink} already exists`);
-  }
+	if (existingRecipe) {
+		throw HttpError(409, `${drink} already exists`);
+	}
 
-  const result = await Coctail.create({ ...req.body, owner });
-  res.status(201).json(result);
+	const result = await Coctail.create({ ...req.body, owner });
+	res.status(201).json({ message: "Recipe added successfully", result });
 };
 
 export { add };
--- controllers/myRecipes-controller/getAll.js
+++ controllers/myRecipes-controller/getAll.js
@@ -1,22 +1,26 @@
 import { Coctail } from "../../models/coctail.js";
+import { responseItems } from "../../constants/controllers-constants.js";
 
 const getAll = async (req, res) => {
-  const { _id: owner } = req.user;
+	const { _id: owner } = req.user;
 
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
+	const { page = 1, limit = 10 } = req.query;
+	const skip = (page - 1) * limit;
 
-  const totalRecipes = await Coctail.countDocuments({ owner });
-  const totalPages = Math.ceil(totalRecipes / limit);
+	const totalRecipes = await Coctail.countDocuments({ owner });
+	const totalPages = Math.ceil(totalRecipes / limit);
 
-  const results = await Coctail.find({ owner }).skip(skip).limit(limit);
+	const results = await Coctail.find({ owner })
+		.select(responseItems)
+		.skip(skip)
+		.limit(limit);
 
-  res.json({
-    totalRecipes,
-    totalPages,
-    currentPage: page,
-    results,
-  });
+	res.json({
+		totalRecipes,
+		totalPages,
+		currentPage: page,
+		results,
+	});
 };
 
 export { getAll };
