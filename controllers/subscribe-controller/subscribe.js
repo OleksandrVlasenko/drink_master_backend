4667091efb3103df8e22531c52c6cdc4223e3259
Helen <bitit333@gmail.com>
2023-08-31 08:48:50 +0300

    subscribe without email,+helpers/sendEmail.js

--- controllers/subscribe-controller/subscribe.js
+++ controllers/subscribe-controller/subscribe.js
@@ -1,3 +1,25 @@
-async function subscribe(req, res) {}
+// import { sendEmail } from "../../helpers/index.js"
+import { Subscriber } from "../../models/subscribe.js"
+import { HttpError } from "../../helpers/HttpError.js"
+
+async function subscribe(req, res) { 
+    const { email } = req.body;
+    const subscriber = await Subscriber.findOne({ email });
+    console.log(subscriber);
+    if (subscriber) throw HttpError(409, "You are already subscribed");
+
+    const newSubsriber = await Subscriber.create(req.body,
+	);
+
+    // const subscribeEmail = {
+    //     to: email,
+    //     subject: "Subscribe",
+    //     html: `<p> Вы подаисались на ... </p>`
+    // };
+
+    // await sendEmail(subscribeEmail);
+
+    res.json(newSubsriber);
+}
 
 export { subscribe };
