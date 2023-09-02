29be777d212d84655c1c21278e67d582d34ea2f9
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-09-03 02:55:41 +0300

    added add recype

--- models/ingredient.js
+++ models/ingredient.js
@@ -7,34 +7,16 @@ const ingredientSchema = new Schema(
 	{
 		title: { type: String, required: true },
 		ingredientThumb: { type: String, default: "" },
+		measure: {type: String, required: true}
 	},
 	{ versionKey: false, timestamps: true },
 );
 
-ingredientSchema.pre("findByIdAndUpdate", handleUpdateValidate);
-
 ingredientSchema.post("save", handleMongooseError);
-
-ingredientSchema.post("findByIdAndUpdate", handleMongooseError);
-
-const emptySchema = Joi.object()
-	.min(1)
-	.messages({ "object.min": "Missing fields" });
-
-const isValidId = Joi.object({
-	favorite: Joi.boolean().required().messages({
-		"any.required": "missing field favorite",
-		"boolean.base": "Must be boolean type",
-	}),
-});
-
-const ingredientSchemaJoi = Joi.object({});
 
 const Ingredient = model("ingredient", ingredientSchema);
 
 export {
 	Ingredient,
-	emptySchema,
-	ingredientSchemaJoi,
-	isValidId,
+	ingredientSchema,
 };
--- routes/api/myRecipes-router.js
+++ routes/api/myRecipes-router.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { myRecipesController } from "../../controllers/myRecipes-controller/index.js";
-import { validateBody, authenticate } from "../../middlewares/index.js";
+import {
+	validateBody,
+	authenticate,
+	isEmptyBody,
+	upload,
+	jsonParse,
+} from "../../middlewares/index.js";
 import { userRecipeAddSchemaJoi } from "../../models/cocktail.js";
 
 const router = express.Router();
@@ -8,8 +14,11 @@ const router = express.Router();
 router.post(
 	"/",
 	authenticate,
+	upload.single("imageOfRecipe"),
+	isEmptyBody,
+	jsonParse,
 	validateBody(userRecipeAddSchemaJoi),
-	myRecipesController.add
+	myRecipesController.add,
 );
 
 router.delete("/:id", authenticate, myRecipesController.removeById);
