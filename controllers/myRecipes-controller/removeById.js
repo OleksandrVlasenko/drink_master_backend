60b67aac45b67fc344828e8307d118a924a5a71e
ArtemBielyi <kolhozreshaet@gmail.com>
2023-09-06 13:41:55 +0300

    fix recipe remove

--- controllers/myRecipes-controller/removeById.js
+++ controllers/myRecipes-controller/removeById.js
@@ -1,13 +1,28 @@
 import { Cocktail } from "../../models/cocktail.js";
 import { HttpError } from "../../helpers/HttpError.js";
 
-const removeById = async (req, res) => {
+const removeById = async (req, res, next) => {
 	const { id } = req.params;
-	const recipes = await Cocktail.findByIdAndRemove(id);
-	if (!recipes) {
-		throw HttpError(400, "Not found");
+	const { _id: user } = req.user;
+
+	try {
+		const deletedDrink = await Cocktail.findOneAndDelete({
+			$and: [{ _id: id }, { owner: user }],
+		});
+		if (!deletedDrink) {
+			throw HttpError(
+				200,
+				"You do not have permission to delete this recipe or recipe not found"
+			);
+		}
+		console.log(deletedDrink);
+
+		res.json({
+			message: `${deletedDrink.drink} has been deleted`,
+		});
+	} catch (error) {
+		next(error);
 	}
-	res.status(201).json({ message: "Delete success" });
 };
 
 export { removeById };
